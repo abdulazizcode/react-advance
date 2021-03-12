5d6c43788a0b6d7d69613e43f93575f8892c3b77
abdulazizcode <tajikcoder@gmail.com>
2021-03-12 16:59:21 +0800

    [ADD]- useState displaying from arr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /*import Setup from "./tutorial/useState/final/error-exsample"; */
-import Setup from "./tutorial/useState/useState-basics";
+/*import Setup from "./tutorial/useState/useState-basics"; */
+import Setup from "./tutorial/useState/useState-array";
 
 function App() {
   return (
