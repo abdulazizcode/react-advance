5ef510508d15a6568547b26448eee8b253d051e0
abdulazizcode <tajikcoder@gmail.com>
2021-03-12 18:08:01 +0800

    [ADD]- useState counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 /*import Setup from "./tutorial/useState/final/error-exsample"; */
 /*import Setup from "./tutorial/useState/useState-basics"; */
 /*import Setup from "./tutorial/useState/useState-array"; */
-import Setup from "./tutorial/useState/useState-objects";
+/*import Setup from "./tutorial/useState/useState-objects"; */
+import Setup from "./tutorial/useState/useState-counter";
 
 function App() {
   return (
